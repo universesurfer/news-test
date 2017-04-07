f997267c328567e7d2ab8b66ff0c7b37e3da0e83
Nicholas Rodman <gitnicholas@gmail.com>
2017-04-06 20:48:05 -0700

    switched from Angular 2 maps to Vanilla Google Maps API - fixed zoom

--- client/aether-news/src/app/map/map.component.ts
+++ client/aether-news/src/app/map/map.component.ts
@@ -2,24 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 
 import { AgmCoreModule } from "angular2-google-maps/core";   //Google Maps
-import { MapsAPILoader } from 'angular2-google-maps/core';
-import { SebmGoogleMap, SebmGooglePolyline, SebmGooglePolylinePoint } from 'angular2-google-maps/core';
+// import { MapsAPILoader, LatLngLiteral, SebmGoogleMap, SebmGoogleMapPolygon } from 'angular2-google-maps/core';
+declare var google: any;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
 
   lat: number = 0;
   lng: number = 0;
   zoom: number = 2;
 
+  // google.charts.load('current', {'packages':['geochart']});
+  //     google.charts.setOnLoadCallback(drawRegionsMap);
+
 
   constructor() { }
 
+
   ngOnInit() {
+
+      let mapOptions = {
+            center: new google.maps.LatLng(0,0),
+            zoom: 3
+        };
+
+        let map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
   }
 
 }
