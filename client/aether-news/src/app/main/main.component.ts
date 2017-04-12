173916f6edab465a4a0fa0ea895d35df38c4a727
Nicholas Rodman <gitnicholas@gmail.com>
2017-04-12 13:34:31 -0700

    retrieving top BBC articles oninit

--- client/aether-news/src/app/main/main.component.ts
+++ client/aether-news/src/app/main/main.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from "@angular/router";
+import { Component, OnInit, NgZone } from '@angular/core';
+import { NewsApiService } from '../service/news-api.service';
+import { Router, RouterModule } from '@angular/router';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-main',
@@ -8,9 +10,28 @@ import { RouterModule } from "@angular/router";
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  private bbcJSON: any;
+
+  constructor(
+    private newsAPI: NewsApiService,
+    private ngZone: NgZone,
+    private http: Http
+  ) { }
 
   ngOnInit() {
-  }
+
+    this.newsAPI.getBBC()
+      .subscribe((res: Response) =>  {
+        this.ngZone.run(()=>{
+          this.bbcJSON = res;
+          console.log(this.bbcJSON.articles);
+  });
+});
+
+
+
+
+}
+
 
 }
